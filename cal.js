0d1a96ba0e550475c2ccfbaa9f0d704ae0e96909
sai-nandhini <131382459+sai-nandhini@users.noreply.github.com>
2023-05-10 19:40:14 +0530

    Add files via upload
    calculator comited

---
+++ cal.js
@@ -0,0 +1,46 @@
+// let outputScreen = document.getElementById("output-screen");
+
+// function display(num){
+//     outputScreen.value += num;
+// }
+
+// function calculate(){
+//     try {
+//         outputScreen.value = eval(outputScreen.value);
+//     } catch (error) {
+//         alert("error")
+//     }
+// }
+
+// function Clear()
+// {
+//     outputScreen.value = "";
+// }
+
+// function del()
+// {
+//     outputScreen.value = outputScreen.value.slice(0,-1);
+// }
+
+let outputScreen = document.getElementById("output-screen");
+
+function display(num){
+    outputScreen.value += num;
+}
+
+function calculate(){
+    try {
+        outputScreen.value = eval(outputScreen.value)
+    } catch (error) {
+        alert("Error")
+        
+    }
+}
+
+function Clear(){
+    outputScreen.value ="";
+}
+
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
